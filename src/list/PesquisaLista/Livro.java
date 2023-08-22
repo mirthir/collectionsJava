bd75fd60b386be3a49f867d2fddc6392545a3cfa
mirthir <46246896+mirthir@users.noreply.github.com>
2023-08-22 15:47:34 -0300

    varios exemplos java

---
+++ src/list/PesquisaLista/Livro.java
@@ -0,0 +1,39 @@
+package list.PesquisaLista;
+
+public class Livro {
+    //atributos
+    public String titulo;
+    public String autor;
+    public int anoPublicacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
