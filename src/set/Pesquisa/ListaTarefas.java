5889848c63b45db67e3619c4e06e0927b760ce7a
mirthir <46246896+mirthir@users.noreply.github.com>
2023-08-23 16:15:25 -0300

    set interface lista tarefa

---
+++ src/set/Pesquisa/ListaTarefas.java
@@ -0,0 +1,117 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaTarefas {
+    //atributos
+    private Set<Tarefa> tarefaSet;
+
+    public ListaTarefas() {
+        this.tarefaSet = new HashSet<>();
+    }
+    public void adicionarTarefa(Tarefa tarefa) {
+        tarefaSet.add(tarefa);
+    }
+    public void removerTarefa(String descricao) {
+        Tarefa tarefaParaRemover = null;
+        if(!tarefaSet.isEmpty()) {
+            for(Tarefa t : tarefaSet) {
+                if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                    tarefaParaRemover = t;
+                    break;
+                }
+            }
+            tarefaSet.remove(tarefaParaRemover);
+        } else {
+            System.out.println("O conjunto está vazio!");
+        }
+        if(tarefaParaRemover == null) {
+            System.out.println("Tarefa não encontrada!");
+        }
+    }
+
+    public void exibirTarefas() {
+        if(!tarefaSet.isEmpty()) {
+            System.out.println(tarefaSet);
+        } else {
+            System.out.println("Tarefa não encontrada!");
+        }
+    }
+    public int contarTarefa() {
+        return tarefaSet.size();
+    }
+    public Set<Tarefa> obterTarefasConcluidas() {
+        Set<Tarefa> tarefasConcluidas = new HashSet<>();
+        for(Tarefa t : tarefaSet) {
+            if(t.isConcluida()) {
+                tarefasConcluidas.add(t);
+            }
+        }
+        return tarefasConcluidas;
+    }
+    public Set<Tarefa> obterTarefasPendentes() {
+        Set<Tarefa> tarefasNaoConcluidas = new HashSet<>();
+        for(Tarefa t : tarefaSet) {
+            if(!t.isConcluida()) {
+                tarefasNaoConcluidas.add(t);
+            }
+        }
+        return tarefasNaoConcluidas;
+    }
+    public void marcarTarefaConcluida(String descricao) {
+        for(Tarefa t : tarefaSet) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                t.setConcluida(true);
+            }
+        }
+    }
+    public void marcarTarefaPendente(String descricao) {
+        Tarefa tarefaParaMarcarComoPendente = null;
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaMarcarComoPendente = t;
+                break;
+            }
+        }
+        if(tarefaParaMarcarComoPendente != null) {
+            tarefaParaMarcarComoPendente.setConcluida(true);
+            System.out.println("Tarefa marcada como pendente: " + tarefaParaMarcarComoPendente.getDescricao());
+        } else {
+            System.out.println("Tarefa não encontrada na lista.");
+            }
+        }
+    public void limparListaTarefas() {
+        if(tarefaSet.isEmpty()) {
+            System.out.println("A lista já está vazia!");
+        } else {
+            tarefaSet.clear();
+        }
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        listaTarefas.adicionarTarefa(new Tarefa("Tarefa 1", false));
+        listaTarefas.adicionarTarefa(new Tarefa("Tarefa 2", false));
+        listaTarefas.adicionarTarefa(new Tarefa("Tarefa 3", false));
+        listaTarefas.adicionarTarefa(new Tarefa("Tarefa 4", false));
+        listaTarefas.exibirTarefas();
+
+        listaTarefas.removerTarefa("Tarefa 55");
+        listaTarefas.exibirTarefas();
+
+        System.out.println("Existem " + listaTarefas.contarTarefa() + " na lista de tarefas.");
+
+        System.out.println(listaTarefas.obterTarefasConcluidas());
+
+        System.out.println(listaTarefas.obterTarefasPendentes());
+
+        listaTarefas.marcarTarefaConcluida("Tarefa 2");
+        listaTarefas.marcarTarefaConcluida("Tarefa 1");
+        listaTarefas.exibirTarefas();
+        listaTarefas.limparListaTarefas();
+        listaTarefas.exibirTarefas();
+    }
+}
+
